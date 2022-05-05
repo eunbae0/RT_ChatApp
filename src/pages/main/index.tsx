654e78d34dfce09a8a6bd7a79b196bf38c3a4fe1
은배 <geb9211@gmail.com>
2022-05-05 22:27:40 +0900

    ♻️ UserList 컴포넌트 Main에서 분리

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -7,55 +7,8 @@ import SearchIcon from '@mui/icons-material/Search';
 import "./main.scss";
 
 import {UserData} from '../../router';
+import UserList from '../../components/userList';
 
-function UserList({data, userInfoData, setIsClickedAddFriendsBtn, property}:{data:UserData; userInfoData: UserData; setIsClickedAddFriendsBtn: React.Dispatch<React.SetStateAction<boolean>>; property: string}): JSX.Element {
-  const onClickAddFriendsBtn = async () => {
-    if (userInfoData.uid !== data.uid) { // !==기본, 테스트시 ===사용하므로 유의
-      try {
-        await setDoc(doc(db, "Users", `${userInfoData.uid}`, "Friends", `${data.uid}`), {
-          uid: data.uid,
-          userName: data.userName,
-          photoURL: data.photoURL
-        });
-        setIsClickedAddFriendsBtn(true); // 친구목록 실시간 업데이트 구현
-        alert('추가되었습니다')
-      } catch (error) {
-        console.log(error);
-      }
-    } else {
-      alert('본인은 친구추가를 할 수 없습니다.')
-    }
-  };
-  const onClickFriendsDeleteBtn = async () => {
-    const ok = window.confirm('정말 삭제하시겠습니까? 채팅 내역이 사라지며, 되돌릴 수 없습니다.');
-    if (ok) {
-      await deleteDoc(doc(db, "Users", `${userInfoData.uid}`, "Friends", `${data.uid}`));
-      setIsClickedAddFriendsBtn(true); // 친구목록 실시간 업데이트 구현
-      alert('삭제되었습니다.')
-    }
-  };
-
-  switch (property) {
-    case 'findUser':
-      return (
-        <div className="UserListBox">
-          <img className="UserListImg" src={data.photoURL} />
-          <div className="UserListName">{data.userName}</div>
-          <div className="AddFriendsBtn" onClick={onClickAddFriendsBtn}>친구추가하기</div>
-        </div>
-      );
-    case 'friendsList':
-      return (
-        <div className="UserListBox">
-          <img className="UserListImg" src={data.photoURL} />
-          <div className="UserListName">{data.userName}</div>
-          <div className="AddFriendsBtn">대화하기</div>
-          <div className="AddFriendsBtn" onClick={onClickFriendsDeleteBtn}>삭제하기</div>
-        </div>
-      );
-    default: return(<></>);
-  }
-}
 function Main({userInfoData}: {userInfoData: UserData}) {
   const [findUser, setFindUser] = useState<string>('');
   const [userData, setUserData] = useState<Array<UserData>>([]);
@@ -139,3 +92,4 @@ export default Main;
 // 친구 삭제하기 기능
 // 스켈레톤 ui
 // 전체 디자인 구조짜기
+// 리액트18
